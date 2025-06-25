71313b58232acf69b6e015ac8fe11085bd1cf40a
stc516 <stcarroll4@gmail.com>
2025-06-25 13:19:56 -0700

    firebase update

--- app/app/firebaseConfig.js
+++ app/app/firebaseConfig.js
@@ -1,6 +1,6 @@
-// firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAwPyJpBNOJibA-Y4ChyYUHNf7cXYSFSZ0",
@@ -14,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, db };
+export { app, db, auth };
